bfaf4d0c8ddae473bd62a6c28759b666f92116b1
Admin <Admin@TEB127118>
2024-02-28 09:55:09 +0100

    Add project files.

---
+++ Uppgift 6.4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Uppgift_6._1
+{
+    class Program
+    {
+
+        static double Potens(int bas, int exponent)
+        {
+            return Math.Pow(bas, exponent);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in din bas och exponent såhär: 5, 2");
+            int[] numbers = Console.ReadLine().Split(',').Select(x => int.Parse(x.Trim())).ToArray();
+
+            Console.WriteLine($"{numbers[0]}^{numbers[1]} = {Potens(numbers[0], numbers[1])}");
+        }
+    }
+}
